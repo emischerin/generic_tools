da024d0ef51b5f6c0d41ebc1e29802eb3aa1622c
Emischerin <e.mischerin@gmail.com>
2023-02-08 02:19:03 +0300

    ready to move runtime to new mechanism

--- function_relocation/function_relocation/function.cpp
+++ function_relocation/function_relocation/function.cpp
@@ -1,6 +1,27 @@
 #include "function.h"
 namespace generic_tools {
 	namespace runtime {
+		uint8_t* function::RecalculateFunctionAddr(uint8_t* addr)
+		{
+			uint8_t* current_addr = (addr + 1);
+			uint8_t* instruction_end = current_addr + sizeof(int);
+			uint8_t* new_addr = 0;
+			uint8_t* tmp = current_addr;
+			uint8_t bit_step = 0;
+			uint32_t instruction_args = 0;
+			uint8_t* result = 0;
 
+			while (tmp < instruction_end) {
+				uint32_t tmp_val = *tmp;
+				tmp_val = tmp_val << bit_step;
+				instruction_args = instruction_args | tmp_val;
+				bit_step += 8;
+				tmp++;
+			}
+
+			result = current_addr + instruction_args + 5;
+
+			return result;
+		}
 	}
 }
--- function_relocation/function_relocation/function.h
+++ function_relocation/function_relocation/function.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <cstdint>
 namespace generic_tools {
 	namespace runtime {
 		class function
 		{
+		public:
+			uint8_t* RecalculateFunctionAddr(uint8_t* addr);
 		};
 	}
 }
