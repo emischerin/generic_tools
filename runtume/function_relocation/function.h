c6ec2901bf2816869a9df463d4a9b7967885a3eb
Emischerin <e.mischerin@gmail.com>
2023-02-08 03:50:46 +0300

    added jmp opcodes

--- runtume/function_relocation/function.h
+++ runtume/function_relocation/function.h
@@ -44,7 +44,29 @@ namespace generic_tools {
 
 			const std::unordered_set<uint8_t> _return_opcodes = { 0xC3,0xC2,0xCA,0xCB };
 			const std::unordered_set<uint8_t> _legacy_opcodes = { 0x66,0xF2,0xF3 };
-
+			const std::unordered_set<uint8_t> _call_opcodes = {};
+			const std::unordered_set<uint8_t> _jmp_opcodes = 
+			{
+				0x70, /*JO Jb*/
+				0x71,/*JNO Jb*/
+				0x72,/*JB/JNAE/JC Jb*/
+				0x73,/*JNB/JAE/JNC Jb*/
+				0x74,/*JZ/JE Jb*/
+				0x75,/*JNZ/JNE Jb*/
+				0x76,/*JBE/JNA Jb*/
+				0x77,/*JNBE/JA Jb*/
+				0x78,/*JS Jb*/
+				0x79,/*JNS Jb*/
+				0x7A,/*JP/JPE Jb*/
+				0x7B,/*JNP/JPO Jb*/
+				0x7C,/*JL/JNGE Jb*/
+				0x7D,/*JNL/JGE Jb*/
+				0x7E,/*JLE/JNG Jb*/
+				0x7F,/*JNLE/JG Jb*/
+				0xE9, /**/
+				0xEA,/**/
+				0xEB,/**/
+			};
 			void* _first_opcode_ptr = nullptr;
 			std::vector<uint8_t> _data;
 		};
